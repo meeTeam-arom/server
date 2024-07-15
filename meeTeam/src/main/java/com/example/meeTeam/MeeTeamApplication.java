5fd9582b6734810131b292da5f396bb20911f51f
LeeDongHoon <likelion.leedh@gmail.com>
2024-07-15 19:35:59 +0900

    first commit

---
+++ meeTeam/src/main/java/com/example/meeTeam/MeeTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.meeTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeTeamApplication.class, args);
+	}
+
+}
---
+++ meeTeam/src/test/java/com/example/meeTeam/MeeTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meeTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
