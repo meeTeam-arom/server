99612d36f9901085b36daabf3becbf09ab0ae392
yongsik <wkdrndydtlr@naver.com>
2024-07-22 20:05:22 +0900

    OrderProduct 추가

---
+++ meeTeam/src/main/java/com/example/meeTeam/orders/OrderProduct.java
@@ -0,0 +1,28 @@
+package com.example.meeTeam.orders;
+
+
+import com.example.meeTeam.chatroom.Chatroom;
+import com.example.meeTeam.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "OrderProduct")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class OrderProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "prouct_id")
+    private Products product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Orders order;
+}
