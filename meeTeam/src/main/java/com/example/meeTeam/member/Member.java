05c1163cd17a05867ec474d29ddee8443f7ab5ef
LeeDongHoon <likelion.leedh@gmail.com>
2024-07-22 20:00:05 +0900

    [chore] 엔티티 필드 수정

--- meeTeam/src/main/java/com/example/meeTeam/member/Member.java
+++ meeTeam/src/main/java/com/example/meeTeam/member/Member.java
@@ -1,65 +1,43 @@
 package com.example.meeTeam.member;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Entity
-@Table(name = {Member})
+@Table(name = "member")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-
 public class Member {
 
     @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @member_name
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String member_name;
+    private String member_name;
 
-    @member_password
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String member_password;
+    private String member_password;
 
-    @member_phone_num
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String member_phone_num;
+    private String member_phone_num;
 
-    @member_email
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String member_email;
+    private String member_email;
 
-    @member_role
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String member_role;
+    private String member_role;
 
-    @member_manner_temp
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private double member_manner_temp;
+    private double member_manner_temp;
 
-    @member_used_coins
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long member_used_coins;
+    private Long member_used_coins;
 
-    @member_earned_coins
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long member_earned_coins;
+    private Long member_earned_coins;
 
-    @created_at
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String created_at;
+    private String created_at;
 
-    @updated_at
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String updated_at;
+    private String updated_at;
 
-    @latitude
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private double latitude;
+    private double latitude;
 
-    @longitude
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private double longitude;
+    private double longitude;
 
-    @available_date
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private String available_date;
+    private String available_date;
 }
