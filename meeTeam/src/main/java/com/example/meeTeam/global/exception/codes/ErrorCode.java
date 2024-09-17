d68d09918ff039d334ca2bc95e793265b6338556
LeeDongHoon <likelion.leedh@gmail.com>
2024-09-17 15:19:00 +0900

    chore : 예외 처리 간소화

--- meeTeam/src/main/java/com/example/meeTeam/global/exception/codes/ErrorCode.java
+++ meeTeam/src/main/java/com/example/meeTeam/global/exception/codes/ErrorCode.java
@@ -9,58 +9,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode implements BaseCode {
 
-    // 가장 일반적인 응답
-    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
-    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
-    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
-
-    /**
-     * ******************************* Global Error CodeList ***************************************
-     * HTTP Status Code
-     * 400 : Bad Request
-     * 401 : Unauthorized
-     * 403 : Forbidden
-     * 404 : Not Found
-     * 500 : Internal Server Error
-     * *********************************************************************************************
-     */
-    // 잘못된 서버 요청
-    BAD_REQUEST_ERROR(HttpStatus.BAD_REQUEST, "G001", "Bad Request Exception"),
-
-    // @RequestBody 데이터 미 존재
-    REQUEST_BODY_MISSING_ERROR(HttpStatus.BAD_REQUEST, "G002", "Required request body is missing"),
-
-    // 유효하지 않은 타입
-    INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "G003", " Invalid Type Value"),
-
-    // Request Parameter 로 데이터가 전달되지 않을 경우
-    MISSING_REQUEST_PARAMETER_ERROR(HttpStatus.BAD_REQUEST, "G004", "Missing Servlet RequestParameter Exception"),
-
-    // 입력/출력 값이 유효하지 않음
-    IO_ERROR(HttpStatus.BAD_REQUEST, "G005", "I/O Exception"),
-
-    // com.google.gson JSON 파싱 실패
-    JSON_PARSE_ERROR(HttpStatus.BAD_REQUEST, "G006", "JsonParseException"),
-
-    // com.fasterxml.jackson.core Processing Error
-    JACKSON_PROCESS_ERROR(HttpStatus.BAD_REQUEST, "G007", "com.fasterxml.jackson.core Exception"),
-
-    // 권한이 없음
-    FORBIDDEN_ERROR(HttpStatus.FORBIDDEN, "G008", "Forbidden Exception"),
-
-    // 서버로 요청한 리소스가 존재하지 않음
-    NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "G009", "Not Found Exception"),
-
-    // NULL Point Exception 발생
-    NULL_POINT_ERROR(HttpStatus.NOT_FOUND, "G010", "Null Point Exception"),
-
-    // @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
-    NOT_VALID_ERROR(HttpStatus.NOT_FOUND, "G011", "handle Validation Exception"),
-
-    // @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
-    NOT_VALID_HEADER_ERROR(HttpStatus.NOT_FOUND, "G012", "Header에 데이터가 존재하지 않는 경우 "),
-
     //인원 초과됨
     OVER_NUMBER_PEOPLE(HttpStatus.BAD_REQUEST,"G013","인원 수가 초과되어 입장 불가능"),
 
